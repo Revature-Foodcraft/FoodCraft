9c998ff4be6b499396f84f97d500d3c3e3e0d87b
Ruslan Balkovyy <ruslan835@revature.com>
2025-03-28 12:32:26 -0400

    create util files for connecting to db and s3, logger and started working on CRUD

---
+++ Backend/Models/model.js
@@ -0,0 +1,45 @@
+const { documentClient } = require('../util/db');
+const { GetCommand, PutCommand, UpdateCommand, ScanCommand, QueryCommand } = require("@aws-sdk/lib-dynamodb");
+const { logger } = require('../util/logger');
+
+const tableName = "FoodCraft"
+
+async function createUser(params) {
+
+}
+
+async function getUser(params) {
+
+}
+
+async function getUserByUsername(params) {
+
+}
+
+async function updateUser(params) {
+
+}
+
+async function createRecipe(params) {
+
+}
+
+async function updateRecipe(params) {
+
+}
+
+async function deleteRecipe(params) {
+
+}
+
+async function createReview(params) {
+
+}
+
+async function getAllRecipes(params) {
+
+}
+
+async function getRecipe(params) {
+
+}
