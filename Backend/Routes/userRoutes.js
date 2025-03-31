71806dd5a48ca1bbfcce32950e7b801f0a96f659
Vincent <vincentlin@gmail.com>
2025-03-31 13:13:15 -0500

    added swagger for API documentation and added cors

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -2,8 +2,113 @@ import express from 'express';
 import {login, register } from '../Controller/userController.js';
 
 const userRouter = express.Router();
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Create a new user and store in the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Required username for the user.
+ *               password:
+ *                 type: string
+ *                 description: Must be at least 8 characters, include 1 capital letter, and 1 special character. Required.
+ *               firstname:
+ *                 type: string
+ *                 description: First name of the user. Optional.
+ *               lastname:
+ *                 type: string
+ *                 description: Last name of the user. Optional.
+ *               email:
+ *                 type: string
+ *                 description: Email address of the user. Optional.
+ *     responses:
+ *       201:
+ *         description: User created successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User Created
+ *                 user:
+ *                   type: object
+ *                   example: userObj
+ *       400:
+ *         description: Bad request due to invalid input.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Error message
+ *       500:
+ *         description: Internal server error occurred.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Failed to create user
+ */
 
 userRouter.post('/users', register);
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Create a new user and store in the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 description: Required username for the user.
+ *               password:
+ *                 type: string
+ *                 description: Must be at least 8 characters, include 1 capital letter, and 1 special character. Required.
+ *     responses:
+ *       201:
+ *         description: Successful successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User Created
+ *                 token:
+ *                   type: string
+ *                   example: jwt_token_here
+ *       400:
+ *         description: Bad request due to invalid input.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Error message
+ */
 userRouter.post('/login', login);
 
 export default userRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,20 @@
 import express from "express"
 import 'dotenv/config';
 import userRouter from "./Routes/userRoutes.js"
+import cors from "cors"
+import swaggerUi from "swagger-ui-express"
+import { swaggerSpec } from "./util/swagger.js";
 
 const app = express();
 const PORT = 5000;
 
-
+app.use(cors());
 app.use(express.json());
-app.use("/",userRouter)
+app.use("/",userRouter);
 
 
 
-app.get("/", (req, res) => {
-
-  res.json(response);
-});
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
