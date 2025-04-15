416c7da04af7db3c93e7b8a051a6873075b7cdda
Ruslan Balkovyy <ruslan835@revature.com>
2025-04-15 09:54:32 -0400

    fix: Change category field in ingredient schema from optional to required

--- Backend/Controller/fridgeController.js
+++ Backend/Controller/fridgeController.js
@@ -5,7 +5,7 @@ export const addIngredientToFridge = async (req, res) => {
   const ingredientSchema = Joi.object({
     id: Joi.string().required(),
     amount: Joi.number().required(),
-    category: Joi.string().optional,
+    category: Joi.string().required,
   });
 
 
