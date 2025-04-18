192c16236a1f9f850168e5bdd58e1ffd7b32a031
Ruslan Balkovyy <ruslan835@revature.com>
2025-04-18 04:14:49 -0400

    setup backend

--- Frontend/src/Components/ProtectedRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export function ProtectedRoute({ isLoggedIn, children }: { isLoggedIn: boolean; children: React.ReactNode }) {
-    if(isLoggedIn || localStorage.getItem("token")){
-        return children
-    }else{
-        return <Navigate to="/login" replace/>;
-    }
-}
