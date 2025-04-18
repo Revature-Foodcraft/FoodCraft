f46bec79662fd92738fb5efcdd98086ec49316ea
Ruslan Balkovyy <ruslan835@revature.com>
2025-04-18 01:59:34 -0400

    feat: update Jest configuration and add file mocks for asset handling; refactor SavedRecipes component and tests

--- Frontend/jest.config.ts
+++ Frontend/jest.config.ts
@@ -18,5 +18,10 @@ module.exports = {
     "setupTests.ts",
     "index.tsx"
     ],
-  setupFilesAfterEnv:['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  }
+
 }
