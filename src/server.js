a13cf37b202f1665aa53ff425ff6f35d9073a411
Ruslan Balkovyy <ruslan835@revature.com>
2025-04-21 08:20:17 -0400

    feat: implement dynamic CORS origin handling in server.js

--- src/server.js
+++ src/server.js
@@ -17,6 +17,22 @@ const corsOptions = {
   credentials: true, // Allow credentials (cookies, headers, etc.)
 };
 
+const allowedOrigins = ['http://localhost:5173', 'http://my-frontend-react-prod.s3-website.us-east-2.amazonaws.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
+
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/", userRouter);
